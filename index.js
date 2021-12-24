36e83f311abbff6ca00bd092f2f7078eede89b5e
SpeedHunter1 <73805411+JsmliYT@users.noreply.github.com>
2021-12-24 06:53:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const config = require("./config.json")
+const mySecret = process.env['TOKEN']
+const keepAlive = require("./server.js")
+
+const client = new Discord.Client({
+    shards: "auto", // 1800+ ,
+    allowedMentions: {
+      parse: ["roles", "users"],
+      repliedUser: false,
+    },
+    failIfNotExists: false,
+    presence: {
+      activity: {
+        name: `Dm me for help | m!help`, 
+        type: "PLAYING", 
+        url: "https://twitch.tv/#"
+      },
+      status: "online"
+    },
+    restTimeOffset: 0,
+    partials: ["CHANNEL", "MESSAGE", "REACTION"],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    ]
+});
+client.modmailDb = new Enmap({
+    name: "Modmail-Database",
+    //dataDir: "./databases/modmail" //create the folder(s)
+});
+client.settings = new Enmap({
+    name: "Settings-Database",
+    //dataDir: "./databases/settings" //create the folder(s)
+});
+
+keepAlive();
+client.login(mySecret)
+client.on("ready", () => {
+    console.log(`${client.user.tag} is now online!`);
+})
+
+require("./modmail")(client);
